31d27d3e65c7d90ac351d0d5dd478524977bf7bd
David Tucker <david@tucker.name>
2017-10-29 09:12:10 -0700

    Test Collection generation with multiple elements

--- test/test_generate.js
+++ test/test_generate.js
@@ -22,8 +22,8 @@ describe('gjtk.generate', function () {
     it('should return a valid FeatureCollection object when provided nothing', function () {
       assert(gjtk.validate.isFeatureCollection(gjtk.generate.FeatureCollection()));
     });
-    it('should return a valid FeatureCollection object when provided a valid Feature', function () {
-      assert(gjtk.validate.isFeatureCollection(gjtk.generate.FeatureCollection(gjtk.random.Feature())));
+    it('should return a valid FeatureCollection object when provided valid Features', function () {
+      assert(gjtk.validate.isFeatureCollection(gjtk.generate.FeatureCollection([gjtk.random.Feature(), gjtk.random.Feature()])));
     });
   });
 
@@ -31,8 +31,8 @@ describe('gjtk.generate', function () {
     it('should return a valid GeometryCollection object when provided nothing', function () {
       assert(gjtk.validate.isGeometryCollection(gjtk.generate.GeometryCollection()));
     });
-    it('should return a valid GeometryCollection object when provided a valid Geometry', function () {
-      assert(gjtk.validate.isGeometryCollection(gjtk.generate.GeometryCollection(gjtk.random.Geometry())));
+    it('should return a valid GeometryCollection object when provided valid Geometries', function () {
+      assert(gjtk.validate.isGeometryCollection(gjtk.generate.GeometryCollection([gjtk.random.Geometry(), gjtk.random.Geometry()])));
     });
   });
 
